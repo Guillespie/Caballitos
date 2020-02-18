26fd6f495652dfbda90803ec8fc2462c4d04def5
Andres Perez <andrespervil@gmail.com>
2020-02-18 19:22:43 +0100

    18.02

--- CarreraDeCaballos/src/controller/FXMLDocumentController.java
+++ CarreraDeCaballos/src/controller/FXMLDocumentController.java
@@ -7,7 +7,6 @@ package controller;
 
 
 import carreradecaballos.animaciones;
-import carreradecaballos.carreraimagenesfx.Image_resources;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,7 +18,7 @@ import java.util.ResourceBundle;
 public class FXMLDocumentController implements Initializable {
 
     @FXML
-    Canvas canva;
+    public Canvas canva;
 
     public GraphicsContext gc;
 
@@ -27,31 +26,38 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         canva.toBack();
         gc = canva.getGraphicsContext2D();
-        gc.drawImage(Image_resources.caballo_estatico,0,animaciones.y_caballo1,110,82);
-        gc.drawImage(Image_resources.caballo_estatico,0,animaciones.y_caballo2,110,82);
-        gc.drawImage(Image_resources.caballo_estatico,0,animaciones.y_caballo3,110,82);
+        gc.drawImage(carreradecaballos.Image_resources.caballo_estatico,0,animaciones.y_caballo1,110,82);
+        gc.drawImage(carreradecaballos.Image_resources.caballo_estatico,0,animaciones.y_caballo2,110,82);
+        gc.drawImage(carreradecaballos.Image_resources.caballo_estatico,0,animaciones.y_caballo3,110,82);
 
     }
 
+    @FXML
     public void insertMasivo(ActionEvent actionEvent) {
         animaciones.comenzar_animacion(gc);
     }
 
+    @FXML
     public void insertOne(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void insertFloat(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void insertString(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void insertInteger(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void borradoMasivo(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void modificadoMasivo(ActionEvent actionEvent) {
     }
 }
